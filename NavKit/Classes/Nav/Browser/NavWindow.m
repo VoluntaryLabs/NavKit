3745f590ae05aa25beeb09edf59ee02b4cdd170a
Steve Dekorte <steve@dekorte.com>
2014-11-23 22:02:38 -0800

    default min window height of 100

--- NavKit/Classes/Nav/Browser/NavWindow.m
+++ NavKit/Classes/Nav/Browser/NavWindow.m
@@ -52,6 +52,7 @@
     [self setCollectionBehavior:NSWindowCollectionBehaviorFullScreenPrimary];
     
     [self setFrameUsingName:[self windowName]];
+    [self setMinSize:NSMakeSize(100, 100)];
 
     return self;
 }
